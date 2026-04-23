bc4f8095e106fb002070f799c8451158e7413168
NDOLI Jean Damascene <ndolijeandamascene@gmail.com>
2026-04-23 11:54:38 +0200

    Rename core/test.py to core/tests.py and clean up

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+class IndexViewTest(TestCase):
+    def test_index_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_index_page_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Cloud Application")
+        self.assertContains(response, "Group 10")
+        self.assertContains(response, "Cloud Computing")
